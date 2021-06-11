2c48609004841fd289a8eabaf19e75cb4091a8ca
fwilchss <wilchss6@gmail.com>
2021-06-11 16:57:40 -0500

    Landing de Harry Potter

---
+++ README.md
@@ -0,0 +1,74 @@
+Evalución frontend
+
+# Revisión de ambiente  
+Se requiere tener instalado node.js, para esto abrimos una consola o termina y escrivimos el siguiente comando node --version,  
+si este regresa error es por que no se tiene instalado node, entonces procedemos a descargrlo de la siguiente ruta https://nodejs.org/es/   
+y daremos click el boton que dice Actual.  
+
+## Clonar proyecto  
+Para clonar el proyecto debemos tener git instalado  
+En caso de no tenerlo consultar la guia para hacerlo : http://rogerdudler.github.io/git-guide/index.es.html  
+Ingresamos a la carpeta donde se clonara el  proyecto y abrimos la consola de git, "Git Bash Here"  
+Ejecutamos el siguiente comando git clone https://github.com/fwilchss/harrypotter.git  
+Entramos al proyecto con cd harrypotter  
+
+## Ambiente REACT  
+Instalamos react:  
+npm install -g create-react-app   
+Actualizamos npm para que descarge node_module:   
+npm update  
+Se enciente el proyecto :  
+npm start  
+
+## Levantar api fake  
+Verificar que estemos dentro de la carpeta harrypotter si no es asi entramos con:   
+cd harrypotter  
+
+En otra terminal levantarmos el servicio de json-server  
+Instalamos Json Server:   
+npm install -g json-server    
+
+Se agrego un elemento objeto a cada archivo json, ya que no esta en el formato que lo requiere json server para poder ejecutarlo   
+Ejemplo: {"staff": "Datos de Archivo"}  
+Tambien se tiene que agregar el elemento id a los registros ya que al guardar con json server marca error si no lo tiene  
+Nota: Esto ya no se tiene que hacer, ya que se corrigio al subir  
+
+Ingresamos a la carperta rest la cual se coloco en la carpeta raiz: cd harrypotter\rest.  
+Abrimos los archivos con json-server:   
+json-server --watch .\hp-students.json  --port 4000,   
+json-server --watch .\hp-staff.json  --port 4001
+
+# Desarrollo del proyecto  
+A continuación se explica como se creo el proyecto inicialmente.  
+
+## Creacion del proyecto en ambiente local  
+Vamos a crear un proyecto nuevo  : npx create-react-app harrypotter;   
+Entramos a la carpeta que se creo, en este caso es harrypotter : cd harrypotter  
+Se enciente el proyecto : npm start  
+Una vez que se levanto el proyecto se empezo a codificar 
+
+### Ambiente de estilos  
+Se agrega bootstrap para react:  
+npm i reactstrap,  
+npm i bootstrap  
+
+### Instalamos iconos :   
+npm i --save @fortawesome/fontawesome-svg-core,  
+npm install --save @fortawesome/react-fontawesome,   
+npm install --save @fortawesome/free-solid-svg-icons,   
+npm install --save @fortawesome/free-brands-svg-icons,     
+npm install --save @fortawesome/free-regular-svg-icons   
+
+### Instalamos SASS: npm install -g sass  
+Para compilar por terminal agregamos el siguiente paquete : npm-install -g node-sass  
+Para compilar y dejar un observador de cambios de Sass a css :  node-sass -w ./src/css -o ./src/css  
+Se importa axios para las consultas de los servicios: npm install axios  
+
+### Intalar redux  
+npm install redux   
+
+### Quitar vulnerabilidades 
+npm audit fix  
+
+  
+
---
+++ src/Registro.js
@@ -0,0 +1,272 @@
+import React from 'react';
+import axios from 'axios';
+import './css/style.css';
+import {Button, Modal, ModalBody,ModalHeader,ModalFooter, FormGroup, Input, Label, Col, Container, Row, Form, Alert} from 'reactstrap'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+class Registro extends React.Component {
+
+    state = {
+        id : '1',
+        name : '',
+        dateOfBirth : new Date(),
+        eyeColour : '',
+        hairColour : '',
+        gender : '',
+        position : '',
+        msn : '',
+        showMsn : 'none',
+        typeMsn : 'alert-danger'
+    }
+
+    handleChange = event => {
+        let name = document.getElementById('name').value;
+        //let dateOfBirth = document.getElementById('dateOfBirth').value;
+        let eyeColour = document.getElementById('eyeColour').value;
+        let hairColour = document.getElementById('hairColour').value;
+        let radioGender = document.getElementsByName("gender");
+        let gender = '';
+        for (var i = 0; i<radioGender.length; i++) {
+            if (radioGender[i].checked == true) {
+                gender = radioGender[i].value;
+            }
+        }
+
+        let radioPosition = document.getElementsByName("position");
+        let position = '';
+        for (var i = 0; i<radioPosition.length; i++) {
+            if (radioPosition[i].checked == true) {
+                position = radioPosition[i].value;
+            }
+        }
+
+        this.setState({ 
+            name: name,
+            //dateOfBirth: dateOfBirth,
+            eyeColour: eyeColour,
+            hairColour:hairColour,
+            gender: gender,
+            position: position
+        });
+    }
+
+    setStudiante =() => {
+        let url = '';    
+
+        if(this.state.position == 1)
+            url = "http://localhost:4000/students";
+        else
+            url = "http://localhost:4001/staff";
+
+        this.setState({ 
+            typeMsn : 'alert-danger'         
+        })    
+                
+
+        if(this.state.name == ''){
+            this.setState({ 
+                msn : 'Agrega el nombre del personaje',
+                showMsn : 'block'                
+            })
+            return;
+        } else {
+            this.setState({ 
+                msn : '',
+                showMsn : 'none'                
+            })
+        }
+
+        if(this.state.dateOfBirth == ''){
+            this.setState({ 
+                msn : 'Agrega el cumpleaños del personaje',
+                showMsn : 'block'                
+            })
+            return;
+        } else {
+            this.setState({ 
+                msn : '',
+                showMsn : 'none'                
+            })
+        }
+
+        if(this.state.eyeColour == ''){
+            this.setState({ 
+                msn : 'Agrega el color de ojos del personaje',
+                showMsn : 'block'
+            })
+            return;
+        } else {
+            this.setState({ 
+                msn : '',
+                showMsn : 'none'
+            })
+        }
+
+        if(this.state.hairColour == ''){
+            this.setState({ 
+                msn : 'Agrega el color de pelo del personaje',
+                showMsn : 'block'
+            })
+            return;
+        } else {
+            this.setState({ 
+                msn : '',
+                showMsn : 'none'
+            })
+        }
+
+        if(this.state.gender == ''){
+            this.setState({ 
+                msn : 'Agrega el genero del personaje',
+                showMsn : 'block'
+            })
+            return;
+        } else {
+            this.setState({ 
+                msn : '',
+                showMsn : 'none'
+            })
+        }
+
+        if(this.state.position == ''){
+            this.setState({ 
+                msn : 'Agrega la posición del personaje',
+                showMsn : 'block'
+            })
+            return;
+        } else {
+            this.setState({ 
+                msn : '',
+                showMsn : 'none'
+            })
+        }
+
+        let dateOfBirth = document.getElementById('dateOfBirth').value;
+
+        axios.post(url, {
+            name: this.state.name,
+            dateOfBirth : dateOfBirth,
+            eyeColour : this.state.eyeColour,
+            hairColour : this.state.hairColour,
+            gender : this.state.gender,
+            position : this.state.position,
+            image : "https://pbs.twimg.com/profile_images/1378007131027251209/qY7wH_2W_400x400.jpg"
+        }).then(res => {
+            //console.info(res);
+            //console.info(res.data);
+            if(res) {
+                if(this.state.position == 1){
+                    this.props.filtroStudent();
+                } else {
+                    this.props.filtroStaff();
+                }
+                
+                this.setState({ 
+                    typeMsn : 'alert-success',      
+                    msn : 'Los datos se guardaron con éxito',
+                    showMsn : 'block'
+                })    
+
+                setTimeout(() => {
+                    this.props.abriModal();
+                }, 3000);
+            }
+        }).catch(
+            function (error) {
+                this.setState({ 
+                    typeMsn : 'alert-danger',      
+                    msn : 'Hubo un error al guardar',
+                    showMsn : 'block'
+                })    
+                console.log('Show error notification!')
+                return Promise.reject(error)
+            }
+        )
+    }
+
+    onChangeFecha = dateOfBirth => {
+        var d = new Date(dateOfBirth);
+        var n = d.getDay();
+        console.log(n)
+
+        this.setState({ 
+            dateOfBirth: dateOfBirth
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button color="success" className="hide" >
+                    Launch demo modal
+                </Button>
+
+                <Modal isOpen={this.props.abierto} size="lg">
+                    <ModalHeader closeButton>
+                        Agregar un personaje
+                    </ModalHeader>    
+                    <ModalBody>
+                        <FormGroup>
+                            <Container>
+                                <Row>
+                                    <Col md={6}>
+                                        <Label>Nombre</Label>
+                                        <Input type="text" className="form-control" id="name" onChange={this.handleChange}></Input>        
+                                    </Col>
+                                    <Col md={6}>
+                                        <Label className="col-md-12">Cumpleaños</Label>
+                                        <DatePicker selected={this.state.dateOfBirth} className="form-control" id="dateOfBirth" onChange={this.onChangeFecha} dateFormat="d-M-yyyy"/>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={6}>
+                                        <Label>Color de ojos</Label>
+                                        <Input type="text" className="form-control" id="eyeColour" onChange={this.handleChange}></Input>        
+                                    </Col>
+                                    <Col md={6}>
+                                        <Label>Color de pelo</Label>
+                                        <Input type="text" className="form-control" id="hairColour" onChange={this.handleChange}></Input>        
+                                    </Col>
+                                </Row>
+                                <Row className="form-group">
+                                    <Col md={6} lg={6}>
+                                        <Label className="form-group labelRadio">Genero</Label>
+                                        <Form className="form-inline">
+                                            <Input type="radio" value="female" id="gender1" name="gender" onChange={this.handleChange}></Input> <Label for="gender1"className="genero">Mujer</Label>
+                                            <Input type="radio" value="male" id="gender2" name="gender" onChange={this.handleChange}></Input> <Label for="gender2" className="genero">Hombre</Label>
+                                        </Form>
+                                    </Col>
+                                    <Col md={6} lg={6}>
+                                        <Label className="form-group labelRadio">Posicion</Label>
+                                        <Form className="form-inline">
+                                            <Input type="radio" value="1" id="position1" name="position" onChange={this.handleChange}></Input> <Label for="position1" className="genero">Estudiante</Label>
+                                            <Input type="radio" value="2" id="position2" name="position" onChange={this.handleChange}></Input> <Label for="position2" className="genero">Staff</Label>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                                <Row >
+                                    <Col md={12} lg={12}>
+                                        <div className={"alert "+this.state.typeMsn} style={{display:this.state.showMsn}}>
+                                            {this.state.msn}
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </FormGroup>
+                    </ModalBody>
+                    <ModalFooter className="text-center">
+                        <Button variant="secondary" onClick={this.props.abriModal}>
+                            Cerrar
+                        </Button>
+                        <Button variant="primary" onClick={this.setStudiante}>
+                            Guardar
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default Registro;
